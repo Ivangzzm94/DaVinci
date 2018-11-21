d94358f630f813968838a7e88138d9c861a258a3
Iván González Montemayor <ivanmontemayor94@gmail.com>
2018-11-20 22:26:47 -0600

    Declaración de variables dentro de while

--- parser.py
+++ parser.py
@@ -167,7 +167,7 @@ def p_statute(p):
 	 | print'''
 
 def p_while(p):
-    '''while : WHILE while_return LPAREN expression RPAREN type_check LBRACE b2 RBRACE end_while'''
+    '''while : WHILE while_return LPAREN expression RPAREN type_check block end_while'''
 
 def p_while_return(p):
     '''while_return :'''
